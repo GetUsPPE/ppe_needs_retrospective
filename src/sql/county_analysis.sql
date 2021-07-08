551cc8b8ee41083983f8da40756bb509ed8bc7bb
Cle Diggins <andrediggins@gmail.com>
2021-07-08 14:16:51 -0700

    minor updates, explanation in email to Matt

--- src/sql/county_analysis.sql
+++ src/sql/county_analysis.sql
@@ -11,6 +11,8 @@ FROM
 WHERE
    r.datecreated IS NOT NULL 
    AND f.county IS NOT NULL 
+   -- remove facilities that were 'merged' as they are not unique facilities and a new facility row was created on merge
+   AND f.mergedintofacilityid is null
 GROUP BY
    f.stateprovince,
    f.county,
